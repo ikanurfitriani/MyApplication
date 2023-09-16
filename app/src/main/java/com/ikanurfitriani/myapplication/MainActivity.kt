9e4e246b3488fcc2e07129da6f9a96c33a9adfc8
ikanurfitriani <ikan07393@gmail.com>
2023-09-16 22:54:34 +0700

    Update aplikasi

--- app/src/main/java/com/ikanurfitriani/myapplication/MainActivity.kt
+++ app/src/main/java/com/ikanurfitriani/myapplication/MainActivity.kt
@@ -1,46 +1,146 @@
 package com.ikanurfitriani.myapplication
 
+// Import library yang akan digunakan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import android.content.res.Configuration
+//import androidx.compose.foundation.layout.fillMaxSize
 import com.ikanurfitriani.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Blok untuk menentukan tata letak aktivitas tempat fungsi composable
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Conversation(SampleData.conversationSample)
+                //Surface(modifier =  Modifier.fillMaxSize()) {
+                    //MessageCard(Message("Ika Nurfitriani", "Jetpack Compose"))
+                //}
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+data class Message(val author: String, val body: String)
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MessageCard(msg: Message) {
+    // Mengatur baris
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        // Menambahkan elemen gambar
+        Image(
+            painter = painterResource(R.drawable.profile_ika_n),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = ""
+        )
+
+        // Mengatur kolom
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp))
+            {
+                Text(text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+// Untuk menampilkan pratinjau tampilan aplikasi
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
     MyApplicationTheme {
-        Greeting("Android")
+        Surface {
+            MessageCard(
+                msg = Message("Ika N", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewConversation() {
+    MyApplicationTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ app/src/main/java/com/ikanurfitriani/myapplication/SampleData.kt
@@ -0,0 +1,72 @@
+package com.ikanurfitriani.myapplication
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Ika N",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Ika N",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "Ika N",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Ika N",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Ika N",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "Ika N",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Ika N",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Ika N",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Ika N",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Ika N",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Ika N",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Ika N",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Ika N",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
